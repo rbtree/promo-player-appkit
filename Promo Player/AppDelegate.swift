2e887f976e59ec1703c2186713e56bdc767464ca
Srđan Marković <srdjan.markovic@rbt.rs>
2019-10-28 17:09:06 +0100

    Initial Commit

---
+++ Promo Player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Promo Player
+//
+//  Created by Srdjan Markovic on 28/10/2019.
+//  Copyright © 2019 Red Black Tree d.o.o. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Promo Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Promo Player
+//
+//  Created by Srdjan Markovic on 28/10/2019.
+//  Copyright © 2019 Red Black Tree d.o.o. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
